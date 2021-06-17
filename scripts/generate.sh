998aadf6564e84349e8d52f9f561dec250fa0b49
Arturo Ojeda <arturo@handy.la>
2021-06-17 11:17:59 -0500

    Testing things out

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -116,7 +116,7 @@ echo "RUN apt-get -y install awscli"
 echo "RUN apt-get -y install zip unzip"
 
 echo "RUN curl -s \"https://get.sdkman.io\" | bash"
-echo "RUN source \"/root/.sdkman/bin/sdkman-init.sh\" "
+
 echo "RUN sdk install java 8u144-zulu"
 
 echo "RUN sdk install grails 2.5.6"
