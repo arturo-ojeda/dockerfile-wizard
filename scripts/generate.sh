b91431fe669ea0064e9fdaf1ee8b9a63690be553
Arturo Ojeda <arturo@handy.la>
2021-06-17 14:33:07 -0500

    Testing things out

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -117,6 +117,9 @@ echo "RUN apt-get -y install zip unzip"
 
 echo "RUN curl -s \"https://get.sdkman.io\" | bash"
 
+echo "RUN chmod a+x \"/root/.sdkman/bin/sdkman-init.sh\" "
+echo "RUN source \"/root/.sdkman/bin/sdkman-init.sh\" "
+
 echo "RUN sdk install java 8u144-zulu"
 
 echo "RUN sdk install grails 2.5.6"
